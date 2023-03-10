61f410d641b8146d2aa6c602c2d0c5f22022e3e1
jiin-coder <fpahstiq123@naver.com>
2023-03-10 11:07:35 +0900

    03 컴포넌트

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,19 @@
 <template>
-  <div>Hello world!!</div>
+  <div>{{ name }}</div>
 </template>
+
+<script>
+export default ({
+  setup() {
+    const name = 'Kim jiae';
+
+    return {
+      name
+    }
+  },
+})
+</script>
+
+<style scoped>
+
+</style>
